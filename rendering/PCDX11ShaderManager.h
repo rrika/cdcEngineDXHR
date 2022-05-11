8863fd34e9df8ce86605f9e23bbf4bb88ef64253
Adam Jensen <adam.jensen@si.det.usa>
2022-05-11 13:49:10 +0100

    refactor PCDX11ShaderManager

--- rendering/PCDX11ShaderManager.h
+++ rendering/PCDX11ShaderManager.h
@@ -5,23 +5,19 @@
 
 namespace cdc {
 
+struct ShaderManagerSub {
+	uint32_t dword0;
+	uint32_t dword4;
+	uint32_t dword8;
+};
+
 class PCDX11ShaderManager {
 	// uint32_t dword0;
-	// uint32_t dword4;
-	// uint32_t dword8;
-	// uint32_t dwordC;
-	// uint32_t dword10;
-	// uint32_t dword14;
-	// uint32_t dword18;
-	// uint32_t dword1C;
-	// uint32_t dword20;
-	// uint32_t dword24;
-	// uint32_t dword28;
-	// uint32_t dword2C;
-	// uint32_t dword30;
-	// uint32_t dword34;
-	// uint32_t dword38;
-	// uint32_t dword3C;
+	// ShaderManagerSub sub4;
+	// ShaderManagerSub sub10;
+	// ShaderManagerSub sub1C;
+	// ShaderManagerSub sub28;
+	// ShaderManagerSub sub34;
 	// uint8_t gap40[1028];
 	// uint8_t gap444[1028];
 	// uint8_t gap848[1028];
@@ -29,21 +25,21 @@ class PCDX11ShaderManager {
 	// uint8_t gap1050[1028];
 public:
 	PCDX11PixelShader *createPixelShader(char *blob, bool takeCopy, bool isWrapped) {
-		// uint32_t a = ++dword4;
-		// uint32_t b = dwordC;
-		// dwordC = a > b ? a : b;
+		// uint32_t a = ++sub4.dword0;
+		// uint32_t b = sub4.dword8;
+		// sub4.dword8 = a > b ? a : b;
 		auto *shader = PCDX11PixelShader::create(blob, takeCopy, /*gap40,*/ &isWrapped, isWrapped);
 		// if (isWrapped)
-		// 	dword8++;
+		// 	sub4.dword4++;
 		return shader;
 	}
 	PCDX11VertexShader *createVertexShader(char *blob, bool takeCopy, bool isWrapped) {
-		// uint32_t a = ++dword10;
-		// uint32_t b = dword18;
-		// dword18 = a > b ? a : b;
+		// uint32_t a = ++sub10.dword4;
+		// uint32_t b = sub10.dword8;
+		// sub10.dword8 = a > b ? a : b;
 		auto *shader = PCDX11VertexShader::create(blob, takeCopy, /*gap444,*/ &isWrapped, isWrapped);
 		// if (isWrapped)
-		// 	dword14++;
+		// 	sub10.dword0++;
 		return shader;
 	}
 };
