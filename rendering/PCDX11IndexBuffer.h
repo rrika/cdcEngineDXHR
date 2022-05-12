0c6a6140e5fa5b69ab5d45b35edd09593ea79528
Adam Jensen <adam.jensen@si.det.usa>
2022-05-12 14:09:33 +0100

    fix includes in PCDX11IndexBuffer.h

--- rendering/PCDX11IndexBuffer.h
+++ rendering/PCDX11IndexBuffer.h
@@ -1,13 +1,13 @@
 #pragma once
+#include <cstdint>
 #include <d3d11_1.h>
 #include "CommonIndexBuffer.h"
-#include "../types.h"
 
 namespace cdc {
 
 class PCDX11IndexBuffer : public CommonIndexBuffer {
 public:
-	virtual uint getStartIndex() = 0;
+	virtual uint32_t getStartIndex() = 0;
 	virtual ID3D11Buffer *getD3DBuffer() = 0;
 	virtual uint32_t getCount() = 0;
 	virtual void method_10() = 0;
@@ -18,7 +18,7 @@ class HackIndexBuffer : public PCDX11IndexBuffer {
 public:
 	HackIndexBuffer(ID3D11Buffer *buffer) : buffer(buffer) {}
 
-	uint getStartIndex() override { return 0; };
+	uint32_t getStartIndex() override { return 0; };
 	ID3D11Buffer *getD3DBuffer() override { return buffer; };
 	uint32_t getCount() override { return 0; };
 	void method_10() override {};
