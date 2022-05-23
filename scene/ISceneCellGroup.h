7e65ca0c81624688654d6a9a1970a440936a4cbe
Adam Jensen <adam.jensen@si.det.usa>
2022-05-23 02:50:54 +0100

    add more methods to SceneCellGroup

--- scene/ISceneCellGroup.h
+++ scene/ISceneCellGroup.h
@@ -1,13 +1,22 @@
 #pragma once
+#include <cstdint>
 
 namespace cdc {
 
+class Scene;
 class SceneCell;
 
 class ISceneCellGroup {
 public:
 	// 23 methods
+	virtual Scene *getScene() = 0; // 4
+	virtual uint32_t getCellCount() = 0; // 8
+	virtual SceneCell *cellByIndex(uint32_t index) = 0; // 10
 	virtual SceneCell *queryPoint(float *point, bool useThisContainer) = 0; // 14
+	virtual float *getOrigin() = 0; // 20
+	virtual void setUserData(void *) = 0; // 38
+	virtual void setName(const char *) = 0; // 48
+	virtual const char *getName() = 0; // 4C
 };
 
 }
--- scene/SceneCellGroup.h
+++ scene/SceneCellGroup.h
@@ -17,11 +17,22 @@ class SceneCellGroup :
 	SceneCell **cells28; // 28
 	// UnitBlobSub50Cells *sub5cells; // 90
 	// CellGroupUserData *userData; // 94
+	float origin[4]; // B0
+	const char *name; // C0
 public:
 	// 22 methods from ISceneCellGroup
-	SceneCell *queryPoint(float *point, bool useThisContainer) { // 14
+	Scene *getScene() override {return scene1C; } // 4
+	uint32_t getCellCount() override {return numCells; } // 8
+	SceneCell *cellByIndex(uint32_t index) override { return cells28[index]; } // 10
+	SceneCell *queryPoint(float *point, bool useThisContainer) override { // 14
 		return SceneCellContainer::queryPoint(point, useThisContainer);
 	}
+	float *getOrigin() override { return origin; } // 20
+	void setUserData(void *) override { // 38
+		// TODO
+	}
+	void setName(const char *newName) override { name = newName; } // 48
+	const char *getName() override { return name; } // 4C
 };
 
 }
