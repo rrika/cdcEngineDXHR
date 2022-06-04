e1470cb07cc33f45585af3f565af4c6e9801ebfe
Adam Jensen <adam.jensen@si.det.usa>
2022-06-04 20:48:46 +0100

    stub ThreadSafeMemHeapAllocator

---
+++ allocator/ThreadSafeMemHeapAllocator.cpp
@@ -0,0 +1,44 @@
+#include "ThreadSafeMemHeapAllocator.h"
+
+namespace cdc {
+
+bool ThreadSafeMemHeapAllocator::isResponsibleForPtr(void*) {
+	// TODO
+	return false;
+}
+
+void ThreadSafeMemHeapAllocator::method_28() {
+	// TODO
+}
+
+void ThreadSafeMemHeapAllocator::method_2C() {
+	// TODO
+}
+
+void *ThreadSafeMemHeapAllocator::alloc(uint32_t, uint32_t) {
+	// TODO
+	return nullptr;
+}
+
+void ThreadSafeMemHeapAllocator::free(void*) {
+	// TODO
+}
+
+void ThreadSafeMemHeapAllocator::method_3C() {
+	// TODO
+}
+
+void ThreadSafeMemHeapAllocator::method_5C() {
+	// TODO
+}
+
+void *ThreadSafeMemHeapAllocator::alignedAlloc(uint32_t, uint32_t, uint32_t) {
+	// TODO
+	return nullptr;
+}
+
+void ThreadSafeMemHeapAllocator::method_64() {
+	// TODO
+}
+
+}
---
+++ allocator/ThreadSafeMemHeapAllocator.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "GameHeapAllocator.h"
+
+namespace cdc {
+
+class ThreadSafeMemHeapAllocator : public GameHeapAllocator {
+public:
+	~ThreadSafeMemHeapAllocator() = default;
+	bool isResponsibleForPtr(void*) override;
+	void method_28() override;
+	void method_2C() override;
+	void *alloc(uint32_t, uint32_t) override;
+	void free(void*) override;
+	void method_3C() override;
+	void method_5C() override;
+	void *alignedAlloc(uint32_t, uint32_t, uint32_t) override;
+	void method_64() override;
+};
+
+}
