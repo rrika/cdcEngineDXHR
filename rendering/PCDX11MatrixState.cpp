8962bd59f1dcd18c64994ead15e5370f8e00828f
Adam Jensen <adam.jensen@si.det.usa>
2022-06-26 14:05:24 +0100

    convert spaces to tabs

--- rendering/PCDX11MatrixState.cpp
+++ rendering/PCDX11MatrixState.cpp
@@ -4,10 +4,10 @@
 namespace cdc {
 
 void PCDX11MatrixState::resize(uint32_t count) {
-    poseData = (PoseData*)renderDevice18->linearAlloc30(
-        /*size=*/ 16 + 64 * count + 16 * count,
-        /*requester=*/ 7);
-    // dword10 = renderDevice18->method_3C();
+	poseData = (PoseData*)renderDevice18->linearAlloc30(
+		/*size=*/ 16 + 64 * count + 16 * count,
+		/*requester=*/ 7);
+	// dword10 = renderDevice18->method_3C();
 }
 
 }
