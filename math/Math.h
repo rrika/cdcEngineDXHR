891c733a4ac14f9af55621248fbba4ace3cb1a4b
Adam Jensen <adam.jensen@si.det.usa>
2022-07-16 19:24:37 +0100

    add Quat/Vector2/Vector3/Vector4

--- math/Math.h
+++ math/Math.h
@@ -6,3 +6,25 @@ bool operator==(const float4x4& m1, const float4x4& m2);
 inline bool operator!=(const float4x4& m1, const float4x4& m2) { return !(m1 == m2); }
 float4x4 transpose(float4x4);
 extern float4x4 identity4x4;
+
+namespace cdc {
+
+class Vector {
+public:
+	union {
+		float vec128[4];
+		struct {
+			float x;
+			float y;
+			float z;
+			float w;
+		};
+	};
+};
+
+class Quat : public Vector {};
+class Vector2 : public Vector {};
+class Vector3 : public Vector {};
+class Vector4 : public Vector {};
+
+}
