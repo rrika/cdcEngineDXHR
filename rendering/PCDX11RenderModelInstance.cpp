0ae5a3faa8ccff27e9de334c6fc68371bdc220d0
Adam Jensen <adam.jensen@si.det.usa>
2022-09-30 22:46:22 +0100

    PCDX11RenderModelInstance: fix prim group loop

--- rendering/PCDX11RenderModelInstance.cpp
+++ rendering/PCDX11RenderModelInstance.cpp
@@ -13,12 +13,20 @@ void PCDX11RenderModelInstance::recordDrawables(IMatrixState *matrixState) {
 		return;
 	bool addToNextScene = false;
 	Mesh *mesh = getRenderModel()->getMesh();
+	// if (!mesh)
+	// 	return; // HACK
+	// if (!mesh->meshTable)
+	// 	return; // HACK
+	if (getRenderModel()->vsSelect == -1)
+		return; // HACK
 	uint32_t tab0index = 0;
 	PoseData *poseData = static_cast<PCDX11MatrixState*>(matrixState)->poseData;
 	for (uint32_t i=0; i<mesh->meshCount; i++) {
 		ModelBatch *sub = &mesh->meshTable[i];
-		for (uint32_t j=0; j<mesh->primGroupCount; j++, tab0index++) {
-			// printf("PCDX11RenderModelInstance::recordDrawables %d %d %d\n", i, j, tab0index);
+		for (uint32_t j=0; j<sub->tab0EntryCount_30; j++, tab0index++) {
+			if (tab0index >= mesh->primGroupCount)
+				return; // HACK: nothing good can come from this
+			// printf("PCDX11RenderModelInstance::recordDrawables %d %d %d %p %p %p %p\n", i, j, tab0index, mesh, sub, mesh->meshTable, mesh->primGroups);
 			PrimGroup *primGroup = &mesh->primGroups[tab0index];
 			// NonPersistentPGData *tab0ext16 = &this->tab0Ext16[tab0index];
 			PersistentPGData *tab0ext128 = &this->tab0Ext128[tab0index];
