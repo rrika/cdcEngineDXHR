4ac86ebb3462258fb4b15a772571e151f48c8813
Adam Jensen <adam.jensen@si.det.usa>
2022-10-28 23:00:53 +0100

    raise SIGTRAP on fatal error in linux build

--- sys/Assert.cpp
+++ sys/Assert.cpp
@@ -1,15 +1,26 @@
 #include "Assert.h"
 #include <cstdio>
 #include <cstdarg>
+#ifdef __linux__
+#include <csignal>
+#endif
 
 namespace cdc {
 
 void FatalError(const char *format, ...) {
+	#ifdef __linux__
+		fputs("\033[31m", stderr); // set RED color
+	#endif
 	va_list args;
 	va_start(args, format);
 	vfprintf(stderr, format, args);
 	va_end(args);
-	fputs("", stderr); // newline
+	#ifdef __linux__
+		fputs("\033[0m\n", stderr); // reset color + newline
+		raise(SIGTRAP);
+	#else
+		fputs("\n", stderr); // newline
+	#endif
 }
 
 }
