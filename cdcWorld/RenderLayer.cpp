b88d772e6e8b9112a67faaa861f8190f1336617d
Adam Jensen <adam.jensen@si.det.usa>
2023-01-24 02:51:39 +0000

    move createRenderDevice2 to cdcRenderLayer::Create

---
+++ cdcWorld/RenderLayer.cpp
@@ -0,0 +1,28 @@
+#include "RenderLayer.h"
+#include "rendering/PCDX11RenderDevice.h"
+#include "rendering/renderdevice.h"
+
+using namespace cdc;
+
+uint32_t g_width = 640;
+uint32_t g_height = 480;
+
+namespace cdcRenderLayer {
+
+void Create(HWND hwnd) {
+	// HACK
+#ifdef _WIN32
+	RECT rect;
+	GetClientRect(hwnd, &rect);
+	g_width = rect.right;
+	g_height = rect.bottom;
+#endif
+
+	if (g_CurrentRenderer == RENDERER_DX9)
+		; // createPCRenderDevice(hwnd, g_width, g_height, 0);
+	if (g_CurrentRenderer == RENDERER_DX11)
+		createPCDX11RenderDevice(hwnd, g_width, g_height, 0);
+	// TODO: ShaderUsageDX11.bin
+}
+
+}
---
+++ cdcWorld/RenderLayer.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <windows.h>
+
+namespace cdcRenderLayer {
+
+void Create(HWND hwnd);
+
+}
