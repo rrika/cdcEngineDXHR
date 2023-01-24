8622a2b950a32e6d620b8539f042eaa039906fca
Adam Jensen <adam.jensen@si.det.usa>
2023-01-24 02:37:36 +0000

    move functions from win32/MainVM.cpp to win32/gamewindow.cpp

--- cdcGameShell/win32/MainVM.cpp
+++ cdcGameShell/win32/MainVM.cpp
@@ -4,6 +4,7 @@
 #include "cdcFile/ArchiveFileSystem.h"
 #include "cdcFile/FileHelpers.h"
 #include "../../game/Main.h"
+#include "cdcGameShell/win32/gamewindow.h"
 #include "imgui/imgui.h"
 #include "cdcLocale/localstr.h"
 #include "../../rendering/BuiltinResources.h"
@@ -75,120 +76,7 @@ bool showSetupIfNecessary(uint32_t unknown, uint32_t showSetupOrConfig) {
 	return showSetup(); // TODO
 }
 
-#if ENABLE_IMGUI
-extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-#endif
-
-LRESULT CALLBACK gameWndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-
-#if ENABLE_IMGUI
-	if (ImGui_ImplWin32_WndProcHandler(hwnd, uMsg, wParam, lParam))
-		return true;
-#endif
-
-	// if (uMsg == WM_DEUS_EX_GCS_FEEDBACK) { /*TODO*/ }
-
-	switch (uMsg)
-	{
-	case WM_CHAR:
-		// TODO
-		break;
-	case WM_SIZE:
-		// TODO
-		break;
-	case WM_ACTIVATE:
-		// TODO
-		break;
-	case WM_KEYUP:
-		// TODO
-		break;
-	case WM_PAINT:
-		// TODO
-		break;
-	case WM_CLOSE:
-		// TODO
-		break;
-	case WM_MOVE: {
-		RECT rect;
-		GetClientRect(hwnd, &rect);
-		if (g_renderDevice)
-			static_cast<PCDX11RenderDevice*>(g_renderDevice)->handleResize(rect.right, rect.bottom);
-		// TODO
-		break;
-	}
-	case 0xBB8:
-		// TODO
-		break;
-	case 0x8000:
-		// TODO
-		break;
-	case WM_SIZING:
-		// TODO
-		break;
-	case WM_SYSKEYDOWN:
-		// TODO
-		break;
-	case WM_SYSCOMMAND:
-		// TODO
-		break;
-	case WM_MOUSEFIRST:
-		// TODO
-		break;
-	case WM_DESTROY: // hack
-		PostQuitMessage(0);
-		return 0;
-	}
-
-	return DefWindowProcW(hwnd, uMsg, wParam, lParam);
-}
-
 DisplayConfig displayConfig;
-HWND hwnd1;
-HWND hwnd2;
-HWND hwnd3;
-void *yellowCursor;
-
-void createWindow() {
-	WNDCLASSW wndClass = { sizeof(wndClass) };
-	wndClass.lpfnWndProc = (WNDPROC)gameWndProc;
-	wndClass.hInstance = GetModuleHandleW(0);
-	wndClass.hIcon = LoadIconW(wndClass.hInstance, (LPCWSTR)0x66);
-	// if ( !wndClass.hIcon )
-	// 	wndClass.hIcon = LoadIconW(0, (LPCWSTR)0x7F00);
-	wndClass.lpszClassName = L"DeusExHRDCE";
-	// wndClass.hbrBackground = (HBRUSH)GetStockObject(5);
-	wndClass.hCursor = LoadCursorW(0, (LPCWSTR)0x7F00); // normal cursor
-
-	if (true) { // HACK
-		ICONINFO ii;
-		auto pointerIcon = LoadIconA(wndClass.hInstance, (LPCSTR)109);
-		GetIconInfo(pointerIcon, &ii);
-		ii.fIcon = 0;
-		ii.xHotspot = 5;
-		ii.yHotspot = 5;
-		wndClass.hCursor = CreateIconIndirect(&ii); // new and improved (yellow) cursor
-	}
-
-	yellowCursor = wndClass.hCursor;
-
-	RegisterClassW(&wndClass);
-
-	wchar_t windowName[] = L"Deus Ex: Human Revolution - Director's Cut";
-
-	hwnd1 = hwnd2 = hwnd3 = CreateWindowExW(
-		0, // 8
-		L"DeusExHRDCE",
-		windowName,
-		WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		640,
-		480,
-		nullptr,
-		nullptr,
-		wndClass.hInstance,
-		nullptr);
-}
 
 uint32_t g_width = 640;
 uint32_t g_height = 480;
@@ -253,7 +141,7 @@ int WinMain2(HINSTANCE hInstance, LPSTR lpCmdLine) {
 
 	showSetupIfNecessary(1, showSetupOrConfig);
 
-	createWindow();
+	GameWindow::Create();
 	if (!createRenderDevice()) {
 		// TODO: Failed to initialize Direct3D with current settings
 	}
---
+++ cdcGameShell/win32/gamewindow.cpp
@@ -0,0 +1,124 @@
+#include "gamewindow.h"
+#include "config.h"
+#include "imgui/imgui.h"
+#include "rendering/PCDX11RenderDevice.h"
+
+using namespace cdc;
+
+HWND hwnd1;
+HWND hwnd2;
+HWND hwnd3;
+void *yellowCursor;
+
+#if ENABLE_IMGUI
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+#endif
+
+namespace GameWindow {
+
+LRESULT CALLBACK RealWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+
+#if ENABLE_IMGUI
+	if (ImGui_ImplWin32_WndProcHandler(hwnd, uMsg, wParam, lParam))
+		return true;
+#endif
+
+	// if (uMsg == WM_DEUS_EX_GCS_FEEDBACK) { /*TODO*/ }
+
+	switch (uMsg)
+	{
+	case WM_CHAR:
+		// TODO
+		break;
+	case WM_SIZE:
+		// TODO
+		break;
+	case WM_ACTIVATE:
+		// TODO
+		break;
+	case WM_KEYUP:
+		// TODO
+		break;
+	case WM_PAINT:
+		// TODO
+		break;
+	case WM_CLOSE:
+		// TODO
+		break;
+	case WM_MOVE: {
+		RECT rect;
+		GetClientRect(hwnd, &rect);
+		if (g_renderDevice)
+			static_cast<PCDX11RenderDevice*>(g_renderDevice)->handleResize(rect.right, rect.bottom);
+		// TODO
+		break;
+	}
+	case 0xBB8:
+		// TODO
+		break;
+	case 0x8000:
+		// TODO
+		break;
+	case WM_SIZING:
+		// TODO
+		break;
+	case WM_SYSKEYDOWN:
+		// TODO
+		break;
+	case WM_SYSCOMMAND:
+		// TODO
+		break;
+	case WM_MOUSEFIRST:
+		// TODO
+		break;
+	case WM_DESTROY: // hack
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProcW(hwnd, uMsg, wParam, lParam);
+}
+
+void Create() {
+	WNDCLASSW wndClass = { sizeof(wndClass) };
+	wndClass.lpfnWndProc = (WNDPROC)RealWindowProc;
+	wndClass.hInstance = GetModuleHandleW(0);
+	wndClass.hIcon = LoadIconW(wndClass.hInstance, (LPCWSTR)0x66);
+	// if ( !wndClass.hIcon )
+	// 	wndClass.hIcon = LoadIconW(0, (LPCWSTR)0x7F00);
+	wndClass.lpszClassName = L"DeusExHRDCE";
+	// wndClass.hbrBackground = (HBRUSH)GetStockObject(5);
+	wndClass.hCursor = LoadCursorW(0, (LPCWSTR)0x7F00); // normal cursor
+
+	if (true) { // HACK
+		ICONINFO ii;
+		auto pointerIcon = LoadIconA(wndClass.hInstance, (LPCSTR)109);
+		GetIconInfo(pointerIcon, &ii);
+		ii.fIcon = 0;
+		ii.xHotspot = 5;
+		ii.yHotspot = 5;
+		wndClass.hCursor = CreateIconIndirect(&ii); // new and improved (yellow) cursor
+	}
+
+	yellowCursor = wndClass.hCursor;
+
+	RegisterClassW(&wndClass);
+
+	wchar_t windowName[] = L"Deus Ex: Human Revolution - Director's Cut";
+
+	hwnd1 = hwnd2 = hwnd3 = CreateWindowExW(
+		0, // 8
+		L"DeusExHRDCE",
+		windowName,
+		WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		640,
+		480,
+		nullptr,
+		nullptr,
+		wndClass.hInstance,
+		nullptr);
+}
+
+}
