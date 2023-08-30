66728c143e32ae02ae30f29b3a868c1d798d5815
Adam Jensen <adam.jensen@si.det.usa>
2023-08-31 00:27:55 +0100

    ScriptObject is a GCObject

--- cdcScript/ScriptObject.h
+++ cdcScript/ScriptObject.h
@@ -1,7 +1,8 @@
 #pragma once
+#include "cdcScript/GCObject.h"
+#include "cdcScript/ScriptType.h" // so RCPtr knows ScriptType is derived from RCObject
 #include "cdcSys/RCObject.h"
 #include "cdcSys/SList.h"
-#include "cdcScript/ScriptType.h" // so RCPtr knows ScriptType is derived from RCObject
 
 namespace cdc {
 
@@ -11,16 +12,21 @@ class ScriptType;
 class Function;
 union DataValue;
 
-class ScriptObject /* : public GCObject */ {
+class ScriptObject : public GCObject {
 	RCPtr<ScriptType> scriptType;
 	uint8_t *m_instance; // C
 
 public:
 	ScriptObject(ScriptType*);
-	virtual ~ScriptObject() = default; // force vtable
 
 	Function* GetFunction(int32_t state, int32_t vtIndex);
 	void CallFunction(Function *func, int32_t numArgs, DataValue *args, void *retVal);
+
+	// virtual methods for GCObject
+	~ScriptObject() override;
+	void MarkChildren() override;
+	void Finalize() override;
+	const char* GetGCName() override;
 
 	uint8_t GetState() {
 		return m_instance ? m_instance[0] : 0;
