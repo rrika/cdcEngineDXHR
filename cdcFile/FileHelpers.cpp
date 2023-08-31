9f60dce0ce190a3ea79994ec3b05fe8529a4b728
Adam Jensen <adam.jensen@si.det.usa>
2023-08-31 13:58:52 +0100

    auto-detect BIGFILE location through registry key of steam installation of DXHRDC

--- cdcFile/FileHelpers.cpp
+++ cdcFile/FileHelpers.cpp
@@ -1,4 +1,8 @@
 #include <cstdlib> // for getenv
+#include <string>
+#ifdef _WIN32
+#include <windows.h>
+#endif
 #include "FileHelpers.h"
 #include "ArchiveFileSystem.h"
 #include "FileUserBufferReceiver.h"
@@ -18,17 +22,41 @@ void createLowerFileSystems() {
 
 ArchiveFileSystem *archiveFileSystem_default;
 
+std::string findBigfile() {
+	if (const char *bigfileEnv = getenv("BIGFILE"))
+		return bigfileEnv;
+#ifdef _WIN32
+	char buffer[512] = {0};
+	DWORD bufferSize = sizeof(buffer);
+	LSTATUS r;
+	r = RegGetValueA(HKEY_LOCAL_MACHINE,
+		"Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Steam App 238010",
+		"InstallLocation",
+		RRF_RT_REG_SZ | RRF_SUBKEY_WOW6464KEY,
+		nullptr,
+		buffer,
+		&bufferSize);
+	if (r == ERROR_SUCCESS) {
+		printf("found installation at %s\n", buffer);
+		return std::string(buffer) + "\\BIGFILE.000";
+	}
+	// There's also the key HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 238010
+	// but it's not clear to me when that is written, and which access flag are needed to read it,
+	// so it will remain unimplemented until a need arises.
+#endif
+	return "";
+}
+
 void createHigherFileSystems() {
-	const char *bigfilePath = getenv("BIGFILE");
-	if (!bigfilePath) {
+	std::string bigfilePath = findBigfile();
+	if (bigfilePath.empty()) {
 		printf("\nspecify path to BIGFILE.000 through BIGFILE environment variable\n\n");
 		exit(1);
 		return;
 	}
-
 	archiveFileSystem_default = new ArchiveFileSystem(lowerFileSystem);
-	bool indexOk = archiveFileSystem_default->readIndex(bigfilePath, 0);
-	
+	bool indexOk = archiveFileSystem_default->readIndex(bigfilePath.c_str(), 0);
+
 	if (!indexOk) {
 		FatalError("Unable to open bigfile BIGFILE.DAT!");
 		exit(1);
