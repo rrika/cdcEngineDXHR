e814dd8cf0453079e8c3188e87ec732b2fea0f35
Adam Jensen <adam.jensen@si.det.usa>
2024-02-24 12:09:28 +0000

    QuatInlines.h

---
+++ cdcMath/QuatInlines.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Math.h"
+
+namespace cdc {
+
+inline Quat operator*(QuatArg quatA, QuatArg quatB) { // line 63
+	return {
+		quatA.w*quatB.x + quatA.x*quatB.w + quatA.y*quatB.z - quatA.z*quatB.y, // i
+		quatA.w*quatB.y - quatA.x*quatB.z + quatA.y*quatB.w + quatA.z*quatB.x, // j
+		quatA.w*quatB.z + quatA.x*quatB.y - quatA.y*quatB.x + quatA.z*quatB.w, // k
+		quatA.w*quatB.w - quatA.x*quatB.x - quatA.y*quatB.y - quatA.z*quatB.z  // 1
+	};
+}
+
+}
