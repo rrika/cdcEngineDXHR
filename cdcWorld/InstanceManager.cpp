aab4840b7fe2cf929b2a6d15284a29ca828a0414
Adam Jensen <adam.jensen@si.det.usa>
2024-02-27 00:45:14 +0000

    InstanceManager: keep chain of instances

--- cdcWorld/InstanceManager.cpp
+++ cdcWorld/InstanceManager.cpp
@@ -2,10 +2,13 @@
 #include "InstanceManager.h"
 
 std::vector<Instance*> InstanceManager::s_instances;
+Instance *InstanceManager::s_chain = nullptr;
 
 Instance *InstanceManager::CreateInstance1() {
 	// TODO
 	Instance *instance = new Instance;
+	instance->next = s_chain;
+	s_chain = instance;
 	instance->flags |= 2;
 	s_instances.push_back(instance);
 	return instance;
@@ -14,6 +17,8 @@ Instance *InstanceManager::CreateInstance1() {
 Instance *InstanceManager::CreateInstance2() {
 	// TODO
 	Instance *instance = new Instance;
+	instance->next = s_chain;
+	s_chain = instance;
 	s_instances.push_back(instance);
 	return instance;
 }
