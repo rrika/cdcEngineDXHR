7968908c916ae710b0731206ee62d41551bd71a9
Adam Jensen <adam.jensen@si.det.usa>
2024-04-04 08:37:10 +0100

    uberobject: indicate virtual methods

--- cdcObjects/UberObject.h
+++ cdcObjects/UberObject.h
@@ -20,9 +20,31 @@ public:
 	UberObjectComposite(Instance *instance, bool deferSectionInit);
 	void CreateSections(Instance *instance);
 	Instance *CreateSectionInstance(Instance *instance, dtp::UberObjectProp::SectionProp *info, uint32_t index);
+
+	virtual ~UberObjectComposite() = default;
+	// virtual void method4();
+	// virtual void method8();
+	// virtual void methodC();
+	// virtual void method10();
+	// virtual void method14();
+	// virtual void method18();
+	// virtual void method1C();
 };
 
 class UberObjectSection : public ObjState {
 public:
 	UberObjectSection(Instance *instance, UberObjectComposite *composite, dtp::UberObjectProp::SectionProp *info, uint32_t index);
+
+	virtual ~UberObjectSection() = default;
+	// virtual void method4();
+	// virtual void method8();
+	// virtual void methodC();
+	// virtual void method10();
+	// virtual void method14();
+	// virtual void method18();
+	// virtual void method1C();
+	// virtual void method20();
+	// virtual void method24();
+	// virtual void method28();
+	// virtual void method2C();
 };
--- game/objects/UberObject_DX3.h
+++ game/objects/UberObject_DX3.h
@@ -8,8 +8,33 @@ void UBEROBJECT_DX3_Init(Instance*, GameTracker*);
 class UberObjectComposite_DX3 : public UberObjectComposite {
 public:
 	UberObjectComposite_DX3(Instance *instance);
+
+	// inherit method4
+	// inherit method8
+
+	// virtual void methodC();
+	// virtual void method10();
+	// virtual void method14();
+	// virtual void method18();
+	// virtual void method1C();
 };
 
 class UberObjectSection_DX3 : public UberObjectSection {
+public:
+	// void method4() override;
+	// void method8() override;
 
+	// inherit methodC
+
+	// void method10() override;
+	// void method14() override;
+	// void method18() override;
+	// void method1C() override;
+	// void method20() override;
+	// void method24() override;
+
+	// inherit method28
+	// inherit method2C
+
+	// virtual void method30();
 };
