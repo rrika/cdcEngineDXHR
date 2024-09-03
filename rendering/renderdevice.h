4d3121c09df828b21ff13944528e4f86025f4d0a
Adam Jensen <adam.jensen@si.det.usa>
2024-09-03 22:37:43 +0100

    render: declare MaterialInstanceParams

--- rendering/renderdevice.h
+++ rendering/renderdevice.h
@@ -3,6 +3,8 @@
 
 namespace cdc {
 
+struct StencilSettings;
+
 struct RenderVertex { // line 294
 	float vX, vY, vZ; uint32_t nDiffuse;
 	float vU, vV;     uint32_t pad[2];
@@ -11,6 +13,15 @@ struct RenderVertex { // line 294
 enum PCRendererType { // line 1755
 	RENDERER_DX9 = 0,
 	RENDERER_DX11 = 1
+};
+
+// not to be confused with MaterialInstanceData
+struct MaterialInstanceParams { // line 491
+	cdc::TextureMap *m_pTextures[4]; // 0
+	float m_depthBoundsMin; // 10
+	float m_depthBoundsMax; // 14
+	StencilSettings *m_pStencilParams; // 18
+	cdc::Vector4 m_shaderConstants[8]; // 20
 };
 
 extern PCRendererType g_CurrentRenderer; // 1761
