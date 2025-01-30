43542bf89aebc6e2e0f1f90a86f65731cb76218a
Adam Jensen <adam.jensen@si.det.usa>
2025-01-30 16:53:21 +0000

    wip

---
+++ cdcCollide/CollideTriAndSphere.cpp
@@ -0,0 +1,15 @@
+#include "collideMB.h"
+
+namespace cdc {
+
+int32_t CollideTriAndSphere( // line 21
+	CPoint *contacts,
+	Vector3Arg p,
+	float radius,
+	MTriangle const& tri,
+	uint8_t adjacencyFlags
+) {
+	//
+}
+
+}
---
+++ cdcCollide/CollideUtils.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "collideMB.h"
+
+namespace cdc {
+
+inline bool TestAlignedBoxAndAlignedBox( // line 77
+	Vector3Arg aMin,
+	Vector3Arg aMax,
+	Vector3Arg bMin,
+	Vector3Arg bMax
+) {
+	return
+		aMax.x > bMin.x &&
+		aMax.y > bMin.y &&
+		aMax.z > bMin.z &&
+		aMin.x > bMax.x &&
+		aMin.y > bMax.y &&
+		aMin.z > bMax.z;
+}
+
+inline BBox CalcXformedBBox(BBox const& A, Matrix const& M) { // line 163
+	// TODO
+	return A;
+}
+
+}
