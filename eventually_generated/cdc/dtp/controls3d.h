d96b386d81f9b3b381ad356abf57ab600ee7c27a
Adam Jensen <adam.jensen@si.det.usa>
2025-03-14 23:15:09 +0000

    sound: rename members of Controls3d

--- eventually_generated/cdc/dtp/controls3d.h
+++ eventually_generated/cdc/dtp/controls3d.h
@@ -4,13 +4,15 @@
 namespace dtp {
 
 struct Controls3d { // line 81
-	float float0;
-	float float4;
-	float float8;
-	float floatC;
+	float distanceOuter; // 0
+	float distanceInner; // 4
+	float pan; // 8
+		// float levelOfWide3D; // when playbackType == 3
+		// float m_2DfrontPan; // when playbackType == 6
+	float levelOfLFE; // C
 	float float10;
 	float float14;
-	uint32_t dword18;
+	uint32_t group; // 18
 	uint16_t playbackType; // 1C
 		// lower eight bit are dtp::SoundPlaybackType
 		// see VoiceImpl::Update3D
